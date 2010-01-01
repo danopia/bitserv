795e3b5f36c4e397adc3907a3c87e2d57dfd83d0
Daniel Danopia <me@danopia.net>
2010-01-01 01:00:00 -0500

    Added a working identify command, and the .dist config file now has a whole new LDAP block

--- bitserv.rb
+++ bitserv.rb
@@ -1,8 +1,46 @@
 require 'yaml'
 require 'socket'
 
+require 'rubygems'
+require 'net/ldap'
+
 config = YAML.load open('bitserv.yaml')
+$config = config
 me = config['hostname']
+
+
+module LDAP
+  Config = $config['ldap']
+  
+  def self.base
+    Config['base']
+  end
+
+  def self.ldap
+    @ldap ||= create_ldap
+  end
+  
+  def self.create_ldap
+    ldap = Net::LDAP.new
+    ldap.host = Config['hostname'] || 'localhost'
+    ldap.port = (Config['port'] || 389).to_i
+    ldap
+  end
+  
+  def self.bind node=nil, pass=nil
+    ldap.auth "#{node},#{base}", pass
+    ldap.bind
+  end
+  
+  def self.user_bind username, password
+    bind Config['auth_pattern'].gsub('{username}', username), password
+  end
+  
+  def self.master_bind
+    bind Config['master_bind']['node'], Config['master_bind']['password']
+  end
+end
+
 
 sock = TCPSocket.new(config['uplink']['hostname'], config['uplink']['port'].to_i)
 
@@ -17,12 +55,16 @@ bots.each do |bot|
   sock.puts "NICK #{bot} 1 #{Time.now.to_i} #{bot} #{me} #{me} 0 +ioS * :Your standard #{bot}, minus any features"
 end
 
+stamps = {}
+
 sock.puts "PING :#{me}"
 
 joined_chan = false
 
 while data = sock.gets
-  parts = data.split ' :', 2
+  puts data
+  
+  parts = data.chomp.split ' :', 2
   args = parts.shift.split ' '
   args << parts.shift if parts.any?
   
@@ -53,7 +95,8 @@ while data = sock.gets
     
     when '~' # timestamp, channel, list
       puts "Got user list for #{args[1]}: #{args[2]}"
-      sock.puts ":#{me} SJOIN #{args[0]} #{args[1]} + :@ChanServ"
+      sock.puts ":#{me} SJOIN #{args[0]} #{args[1]} + :@ChanServ" unless stamps[args[1]]
+      stamps[args[1]] = args[0]
       if args[1] == config['services-channel']
         (bots - ['ChanServ']).each do |bot|
           sock.puts ":#{me} SJOIN #{args[0]} #{args[1]} + :@#{bot}"
@@ -61,8 +104,40 @@ while data = sock.gets
         joined_chan = true
       end
     
+    when 'H' # kick; channel, kickee, message
+      puts "#{origin} kicked #{args[1]} from #{args[0]} (#{args[2]})"
+      if args[1] == 'ChanServ'
+        sock.puts ":#{me} SJOIN #{stamps[args[0]]} #{args[0]} + :@ChanServ"
+        sock.puts ":ChanServ H #{args[0]} #{origin} :REVENGEEEEE!"
+      end
+    
     when ')' # channel, setter, when, topic
       puts "Topic for #{args[0]}: #{args.last}"
+    
+    when '!' # channel, message
+      puts "#{origin} said on #{args[0]}: #{args[1]}"
+      if args[1] =~ /^\001ACTION kicks ChanServ/
+        sock.puts ":ChanServ ! #{args[0]} :ow"
+      end
+      
+      if args[0] == 'NickServ'
+        params = args[1].split
+        cmd = params.shift.downcase
+        case cmd
+          
+          when 'id', 'identify'
+            if LDAP.user_bind origin, params.shift
+              #sock.puts ":OperServ PRIVMSG #services :SOPER: #{origin} as #{origin}"
+              sock.puts ":NickServ NOTICE #{origin} :You are now identified for \002#{origin}\002."
+              #sock.puts ":NickServ NOTICE danopia :2 failed logins since last login."
+              #sock.puts ":NickServ NOTICE danopia :Last failed attempt from: danopia!danopia@danopia-F985FA2D on Jan 01 00:25:26 2010."
+              sock.puts ":NickServ SVS2MODE #{origin} +rd #{Time.now.to_i}"
+            else
+              sock.puts ":NickServ NOTICE #{origin} :Invalid password for \002#{origin}\002."
+            end
+          
+        end
+      end
     
     when 'AO' # 10 1262304315 2309 MD5:1f93b28198e5c6a138cf22cf14883316 0 0 0 :Danopia
       
