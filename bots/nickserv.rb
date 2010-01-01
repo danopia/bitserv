7fc3273e3dd2c6aad2aa49b6c308b6aceb4b7281
Daniel Danopia <me@danopia.net>
2010-01-01 14:51:00 -0500

    Added a NS DROP command. not sure if it will work, but no reason why it shouldn't.

--- bots/nickserv.rb
+++ bots/nickserv.rb
@@ -14,10 +14,7 @@ module BitServ
     end
     
     command 'register', 'Registers a nickname.', 'password', 'email' do |origin, params|
-      dn = $config['ldap']['auth_pattern'].gsub('{username}', origin)
-      dn += ",#{$config['ldap']['base']}"
-      puts dn
-      p params
+      dn = $config['ldap']['auth_pattern'].gsub('{username}', origin) + ",#{$config['ldap']['base']}"
       attrs = {
         :cn => origin,
         :userPassword => params.shift,
@@ -25,15 +22,35 @@ module BitServ
         :objectclass => ['x-bit-ircUser', 'top'],
         :uid => origin
       }
-      p attrs
       
-      p LDAP.master_bind
-      if LDAP.ldap.add :dn => dn, :attributes => attrs
+      LDAP.master_bind
+      LDAP.ldap.add :dn => dn, :attributes => attrs
+      if LDAP.ldap.get_operation_result.code == 0
         $sock.puts ":OperServ ! #{$config['services-channel']} :REGISTER: \002#{origin}\002 to \002#{attrs[:mail]}\002"
         $sock.puts ":NickServ SVS2MODE #{origin} +rd #{Time.now.to_i}"
         $sock.puts ":NickServ B #{origin} :\002#{origin}\002 is not registered to \002#{attrs[:mail]}\002, with the password \002#{attrs[:userPassword]}\002."
       else
         $sock.puts ":NickServ B #{origin} :An error occured while creating your account."
+        puts "Result: #{LDAP.ldap.get_operation_result.code}"
+        puts "Message: #{LDAP.ldap.get_operation_result.message}"
+      end
+    end
+    
+    command 'drop', 'Drops an account registration.', 'nickname', 'password' do |origin, params|
+      if !LDAP.user_bind params[0], params[1]
+        $sock.puts ":NickServ B #{origin} :Invalid password for \002#{params[0]}\002."
+        next
+      end
+      
+      dn = $config['ldap']['auth_pattern'].gsub('{username}', origin) + ",#{$config['ldap']['base']}"
+      LDAP.ldap.delete :dn => dn
+      
+      if LDAP.ldap.get_operation_result.code == 0
+        $sock.puts ":OperServ ! #{$config['services-channel']} :DROP: \002#{params[0]}\002 by \002#{origin}\002"
+        $sock.puts ":NickServ SVS2MODE #{origin} -r+d 0"
+        $sock.puts ":NickServ B #{origin} :\002#{params[0]}\002 has been dropped."
+      else
+        $sock.puts ":NickServ B #{origin} :An error occured while dropping your account."
         puts "Result: #{LDAP.ldap.get_operation_result.code}"
         puts "Message: #{LDAP.ldap.get_operation_result.message}"
       end
