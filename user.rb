8ae9324fe1e82f57a936371d5f47be58c8a8e4e6
Daniel Danopia <me@danopia.net>
2010-01-04 00:02:31 -0500

    Only one more TODO for now! BitServ runs again. I need to set up hooks so that bots can join cahnnels and respond to messages.

--- user.rb
+++ user.rb
@@ -24,10 +24,11 @@ module BitServ
     def check_nick_reg
       @dn = $config['ldap']['auth_pattern'].gsub('{username}', @nick) + ",#{$config['ldap']['base']}"
       
+      # TODO: Goes in a NickServ hook! NOT HERE!
       @entry = LDAP.ldap.search :base => @dn
       if @entry
         @entry = @entry.first
-        $uplink.send ":NickServ", 'B', @nick, "This nickname is registered. Please choose a different nickname, or identify via \002/msg NickServ identify <password>\002."
+        #$uplink.send ":NickServ", 'B', @nick, "This nickname is registered. Please choose a different nickname, or identify via \002/msg NickServ identify <password>\002."
       end
     end
   end
