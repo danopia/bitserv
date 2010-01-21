a5505db047fe2ef3f307b4d5eaa955f1334a59d5
Daniel Danopia <me@danopia.net>
2010-01-20 20:10:36 -0500

    Old changes; just commiting now. Added back a functional GitServ.

--- bots/gitserv.rb
+++ bots/gitserv.rb
@@ -7,11 +7,16 @@ end
 
 module BitServ
   class GitServ < ServicesBot
-    command ['issues'], 'Return the number of open/closed issues.', 'owner/project' do |origin, params|
-      open = load_api('issues', 'list', params.first, 'open')['issues']
-      closed = load_api('issues', 'list', params.first, 'closed')['issues']
+    command 'issues', 'Return the number of open/closed issues.', 'owner/project'
+    command ['list', 'ls'], 'List the projects that a person has.', 'person'
+    command ['info', 'show'], 'Show details about a project.', 'owner/project'
+    command 'network', 'List all the projects in a certain project\'s network.', 'owner/project'
+    
+    def cmd_issues origin, project
+      open = load_api('issues', 'list', project, 'open')['issues']
+      closed = load_api('issues', 'list', project, 'closed')['issues']
       
-      notice origin, "#{params.first} has #{open.size} open and #{closed.size} closed issues."
+      notice origin, "#{project} has #{open.size} open and #{closed.size} closed issues."
       
       if open.any?
         open.sort! {|a, b| b['updated_at'] <=> a['updated_at'] }
@@ -23,22 +28,21 @@ module BitServ
       end
     end
 			
-    command ['list', 'ls'], 'List the projects that a person has.', 'person' do |origin, params|
-			notice origin, "#{params.first}'s Projects"
-      load_api('repos', 'show', params.first)['repositories'].each do |repo|
+    def cmd_list origin, person
+			notice origin, "#{person}'s Projects"
+      load_api('repos', 'show', person)['repositories'].each do |repo|
         notice origin, repo[:name] + (repo[:fork] ? ' (fork)' : '')
       end
     end
 			
-    command ['info', 'show'], 'Show details about a project.', 'owner/project' do |origin, params|
-      info = load_api('repos', 'show', params.first)['repository']
+    def cmd_info origin, project
+      info = load_api('repos', 'show', project)['repository']
       notice origin, "#{info[:owner]}/#{info[:name]}: #{info[:description]} [#{info[:watchers]} watchers, #{info[:forks]} forks, #{info[:open_issues]} open issues]"
     end
 	
-    command ['network'], 'List all the projects in a certain project\'s network.', 'owner/project' do |origin, params|
-      info = load_api('repos', 'show', params.first, 'network')
+    def cmd_network origin, project
+      info = load_api('repos', 'show', project, 'network')
       info['network'].each do |project|
-        p project
         notice origin, "#{project[:owner]}/#{project[:name]}" + (project[:fork] ? '' : ' (origin)') + ": #{project[:description]}"
       end
     end
