1a2ff7c2d5dfafb91922e4a76512c0c45bb26589
Daniel Danopia <me@danopia.net>
2010-01-31 14:55:32 -0500

    Added role listing to GroupServ

--- bots/groupserv.rb
+++ bots/groupserv.rb
@@ -1,6 +1,7 @@
 module BitServ
   class GroupServ < ServicesBot
     command 'list', 'List all registered groups.'
+    command 'roles', 'Manage the roles for a certain group.', 'group'
     
     def cmd_list origin
       notice origin, "*** List of Registered Groups ***"
@@ -9,7 +10,17 @@ module BitServ
       LDAP.search("ou=groups,ou=irc,#{LDAP.base}", {:objectclass => 'x-bit-ircGroup'}, :scope => Net::LDAP::SearchScope_SingleLevel).each do |group|
         notice origin, "#{group.ou.first.ljust 16}| (#{group.member.size}) #{group.description}"
       end
-      notice origin, "*** End List ***"
+      notice origin, "*** End Group List ***"
+    end
+    
+    def cmd_roles origin, group
+      notice origin, "*** Group Roles List for #{group}***"
+      notice origin, "Role            | (Members) Description"
+      notice origin, "----------------+--------------------------"
+      LDAP.search("ou=#{group},ou=groups,ou=irc,#{LDAP.base}", {:objectclass => 'x-bit-ircGroup'}, :scope => Net::LDAP::SearchScope_SingleLevel).each do |role|
+        notice origin, "#{role.ou.first.ljust 16}| (#{role.member.size}) #{role.description}"
+      end
+      notice origin, "*** End Role List ***"
     end
   end
 end
