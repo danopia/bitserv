80c572e2a762afbab926c96c346581cae9145700
Daniel Danopia <me@danopia.net>
2010-01-31 14:46:36 -0500

    Added a makeshift reload command to OperServ

--- bots/operserv.rb
+++ bots/operserv.rb
@@ -1,5 +1,23 @@
 module BitServ
   class OperServ < ServicesBot
-
+    command 'reload', 'Reloads the services.'
+    
+    def cmd_reload origin
+      load 'account.rb'
+      load 'bot.rb'
+      load 'channel.rb'
+      load 'connection.rb'
+      load 'ldap.rb'
+      load 'services.rb'
+      load 'user.rb'
+      
+      load 'bots/chanserv.rb'
+      load 'bots/gitserv.rb'
+      load 'bots/groupserv.rb'
+      load 'bots/nickserv.rb'
+      load 'bots/operserv.rb'
+      
+      load 'protocols/inspircd.rb'
+    end
   end
 end
