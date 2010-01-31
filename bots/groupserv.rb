a6de9e30feeb8bd68a5e5a3903596301217d57ec
Daniel Danopia <me@danopia.net>
2010-01-31 14:52:04 -0500

    Added group listing to GroupServ

--- bots/groupserv.rb
+++ bots/groupserv.rb
@@ -1,5 +1,15 @@
 module BitServ
   class GroupServ < ServicesBot
-
+    command 'list', 'List all registered groups.'
+    
+    def cmd_list origin
+      notice origin, "*** List of Registered Groups ***"
+      notice origin, "Name            | (Members) Description"
+      notice origin, "----------------+--------------------------"
+      LDAP.search("ou=groups,ou=irc,#{LDAP.base}", {:objectclass => 'x-bit-ircGroup'}, :scope => Net::LDAP::SearchScope_SingleLevel).each do |group|
+        notice origin, "#{group.ou.first.ljust 16}| (#{group.member.size}) #{group.description}"
+      end
+      notice origin, "*** End List ***"
+    end
   end
 end
